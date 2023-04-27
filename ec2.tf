e5f3d402be13257a566d8995e45c9fce22198b09
Victor M. Ortiz <vicortiz1993@gmail.com>
2023-04-27 11:30:19 -0400

    private ec2 instance

--- ec2.tf
+++ ec2.tf
@@ -12,3 +12,15 @@ resource "aws_instance" "main" {
 output "ec2_ssh_command" {
   value = "ssh -i TDC-P3.pem ubuntu@ec2-${replace(aws_instance.main.public_ip,".", "-")}.compute-1.amazonaws.com"
 }
+
+resource "aws_instance" "pvtec2" {
+  ami = data.aws_ssm_parameter.instance_ami.value
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.Private[0].id
+  key_name = "TDC-P3"
+  vpc_security_group_ids = ["sg-04d32026d5ac8e4b4", "sg-05ada0822c21140eb"]
+  tags = {
+    "Name" = "${var.default_tags.env}-PVT-EC2"
+  }
+    user_data = base64encode(file("C:/Users/vicor/OneDrive/Documents/codingstudymaterial/VETTECMATERIAL/TDC-P3/user.sh"))
+}
